373f592c1978b0edb892e72caab05dd788e3a68a
Anifat <Harniphat24@gmail.com>
2026-02-03 17:20:07 +0100

    changed bg

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,7 @@
 export default function SideBar({ isNavOpen }) {
   return (
     <aside
-      className={`fixed bg-green-700 top-0 left-0 bottom-0 w-1/2 ${isNavOpen ? "translate-x-0" : "-translate-x-full"} duration-700 md:hidden`}
+      className={`fixed bg-red-500 top-0 left-0 bottom-0 w-1/2 max-w-75 ${isNavOpen ? "translate-x-0" : "-translate-x-full"} duration-700 md:hidden`}
     >
       SideBar
     </aside>
