1a356fd89752f865f29fe16390c6d9f1f0a484f4
Anifat <Harniphat24@gmail.com>
2026-02-05 15:48:26 +0100

    styled description

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -18,5 +18,54 @@ export default function Food() {
     getSingleRecipe();
   }, []);
 
-  return <div>Food{path.id}</div>;
+  return (
+    <div>
+      {/* Image */}
+      <div className="m-4 rounded-2xl overflow-hidden object-fill">
+        <img src={recipe.image} alt=""  />
+      </div>
+
+      {/* Descriptions */}
+      <div>
+        {/* Name */}
+      <h2 className="text-2xl font-bold mb-2 px-5 ">{recipe.name}</h2>
+
+      {/* Information */}
+      <div className="px-6">
+
+      {/*  Ingredients */}
+        <h2 className="mt-4  text-2xl font-semibold">Ingredients:</h2>
+        <ul className="list-decimal px-2">
+        {recipe.ingredients && recipe.ingredients.map((x) => <li className="text-lg">{x}</li>)}
+      </ul>
+
+      {/* Instructions */}
+      <h3 className="mt-5 text-2xl font-semibold">Instructions:</h3>
+      <ul className="list-decimal px-2 mb-5">
+        {recipe.instructions && recipe.instructions.map((x) => <li className="text-lg">{x}</li>)}
+      </ul>
+      <div className="flex justify-between">
+        {/* PrepTime */}
+      <span>Prep time {recipe.prepTimeMinutes}mins</span>
+      {/* CookTimeMinutes */}
+      <span> Cook time {recipe.cookTimeMinutes}mins</span>
+      </div>
+      {/* Difficulty*/}
+      <div className="flex gap-4">
+        <h5>Difficulty</h5>-
+        <span className="">{recipe.difficulty}</span>
+      </div>
+      {/* Cuisine */}
+      <span className="">{recipe.cuisine}</span>
+      {/* Calories Per Serving */}
+      <span className="block">{recipe.caloriesPerServing}</span>
+      {/* Meal type */}
+      <ul className="">
+        <p className="text-lg">Meal Type</p>
+        {recipe.mealType && recipe.mealType.map((x)=> <li>{x}</li>) }
+      </ul>
+      </div>
+      </div>
+    </div>
+  );
 }
