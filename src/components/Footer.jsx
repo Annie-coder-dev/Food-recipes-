08cf1cd225ed6c31dd8b45dcb1720c4355411312
Anifat <Harniphat24@gmail.com>
2026-02-05 01:06:30 +0100

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div className='bg-red-500'>Footer</div>
-  )
+    <footer className="bg-red-500 px-7 py-5">
+      <div className="flex justify-between mb-2.5">
+        <div>
+        <h2 className="text-white font-semiboldbold text-[20px] mb-1.5">Quick Links</h2>
+        <ul>
+          <li className="text-white">Home</li>
+          <li className="text-white">Recipes</li>
+          <li className="text-white">Category</li>
+          <li className="text-white">Support</li>
+        </ul>
+      </div>
+      <div>
+        <h2 className="text-white font-semiboldbold text-[20px] mb-1.5">Popular Categories</h2>
+        <ul>
+          <li className="text-white">Breakfast</li>
+          <li className="text-white">Lunch</li>
+          <li className="text-white">Dinner</li>
+          <li className="text-white">Desserts</li>
+        </ul>
+      </div>
+      </div>
+      <p className="text-white text-center py-4">
+        © 2026 My Food Recipes. All rights reserved.
+      </p>
+    </footer>
+  );
 }
